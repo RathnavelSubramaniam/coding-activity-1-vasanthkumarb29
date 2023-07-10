6bd168f412afbc60f4b57c3416404951907915ca
vasanthkumarb29 <vasanthkumar.b29@gmail.com>
2023-07-10 11:02:40 +0530

    new get added

--- Unit1/Coding Activity 1/index.js
+++ Unit1/Coding Activity 1/index.js
@@ -5,6 +5,9 @@ const port = 8000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/data', (req, res) => {
+    res.send('Hello World with Data!')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
